3e3ead8eaa0dae67c5d4eef54567c32ded32618b
Toma Gela <toma.gela@gmail.com>
2022-02-03 17:34:59 +0100

    start to work at home and about section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeSectionComponent } from './home-section/home-section.component';
+import { AboutSectionComponent } from './about-section/about-section.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeSectionComponent,
+    AboutSectionComponent
   ],
   imports: [
     BrowserModule,
